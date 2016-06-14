480c2f566a0fba9be66282406fa657b5fac01087
Stu Maitland <stuart@mediwikis.com>
2016-06-14 20:11:16 +0100

    Can now get all the wikicode for every page linked from a certain page. Grouped queries to avoid rate limit. Added file write output to verify results

--- wikiscraper.py
+++ wikiscraper.py
@@ -9,21 +9,18 @@ categoryPageTitle="Items"
 
 
 def getlistofpages(wikiUrl,wikiPath,categoryPageTitle):
-
+    correctpages=[]
     r=requests.get("http://"+wikiUrl+wikiPath+"/api.php?action=parse&format=json&page="+categoryPageTitle,verify=False)
     print "the server responded with " + r.reason
     pagejson= r.json()
     #returns list as default, get first item of list
     listofpages = pagejson['parse']['links']
     for page in listofpages:
-        if categoryPageTitle in page['*']:
+        if categoryPageTitle not in page['*']:
             #remove any internationalised links e.g. foo/fr
-            page['*']=""
-        else:
             #url encode to detect page
-            page['*']=str(urllib.quote_plus(page['*']))
-
-    return listofpages
+            correctpages.append(str(urllib.quote_plus(page['*'])))
+    return correctpages
 
 def exportwikicode(code):
     with open("wikicode" + ".txt",'w') as f:
@@ -34,18 +31,26 @@ def exportwikicode(code):
 
 listofpages=getlistofpages(wikiUrl,wikiPath,categoryPageTitle)
 x=[]
+concatTitles=[]
 
-for page in listofpages:
-    r=requests.get("http://"+wikiUrl+wikiPath+"/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+page['*'],verify=False)
-    print page['*']
+for i in range(abs(len(listofpages)/50)):
+    #avoid the rate limit in the next step, group concatenated titles in groups of 50
+    concatTitles.append("|".join(listofpages[i*50:i*50+50]))
+print concatTitles
+
+for pageGroup in concatTitles:
+    r=requests.get("http://"+wikiUrl+wikiPath+"/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+pageGroup,verify=False)
+    print ""
     try:
         # This is a ridiculous way to navigate the json tree, there must be a better way
         # Until then:
         ## By default this returns multiple queries, for multiple pages.
         ## Since only one page will be returned, but its indexed by key: pageid, get the first value 
         ## Get the most recent revision, all text
-        x.append( r.json()['query']['pages'].values()[0]['revisions'][0]['*'])
+        #r.json()['query']['pages'].values()[0]['revisions'][0]['*']
+        for page in r.json()['query']['pages'].values():
+            x.append( page['revisions'][0]['*'])
     except:
-        continue
+        print "error"
 exportwikicode(x)
 print ""
